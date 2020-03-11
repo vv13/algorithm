d867610833d40549cb5af567a54780bbb5cb6594
vv13 <zwhvv13@gmail.com>
2020-03-11 09:55:51 +0800

    solution 5 & 6

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 If you like my directory structure, please see [leetcode-problems-crawler)](https://github.com/vv13/leetcode-problems-crawler).
+
+## TODO
+ [] refactor 006 DP
--- solutions/005.longest-palindromic-substring.medium/solution.py
+++ solutions/005.longest-palindromic-substring.medium/solution.py
@@ -1,7 +1,22 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        sLen = len(s)
+        # 2d array
+        dp = [[False] * sLen for i in range(sLen)]
+
+        start = end = max = 0
+        for j in range(sLen):
+            for i in range(j, -1, -1):
+                if i == j:
+                    dp[i][i] = True
+                elif i + 1 == j:
+                    dp[i][j] = (s[i] == s[j])
+                else:
+                    dp[i][j] = s[i] == s[j] and dp[i+1][j-1]
+                if dp[i][j] and j - i > max:
+                    start = i
+                    end = j
+                    max = j - i
+        return s[start:end + 1]
+
+print(Solution().longestPalindrome('asdfjsadjfkdslajasdfdsa'))
