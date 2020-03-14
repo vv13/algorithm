547f5d3236b5d29e026aa0a052d4f4d740faf8ba
vv13 <zwhvv13@gmail.com>
2020-03-15 01:02:01 +0800

    update 12 13 14

---
+++ solutions/013.roman-to-integer.easy/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        map = {
+            '': 0,
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        prev = ''
+        ret = 0
+        for i in s[::-1]:
+            ret += map[i] * (-1 if map[prev] > map[i] else 1)
+            prev = i
+
+        return ret
+
+print(Solution().romanToInt('MMMIIV'))
