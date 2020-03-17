a6c795901a011dd7aa6c44df1242b17090e7f6a8
vv13 <zwhvv13@gmail.com>
2020-03-17 11:53:58 +0800

    update 18

---
+++ leetcode/018.4sum.medium/test.py
@@ -0,0 +1,7 @@
+from solution import Solution
+
+testcase = [-1,0,1,2,-1,-4]
+# result = Solution().fourSum([-3,-2,-1,0,0,1,2,3], 0)
+# result = Solution().fourSum([-1,0,1,2,-1,-4], -1)
+result = Solution().fourSum([0,4,-5,2,-2,4,2,-1,4], 12)
+print(result)
